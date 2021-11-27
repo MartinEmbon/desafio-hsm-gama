4568cc94f8563d9b13a9c352aea7a5b7c6a1c135
Martincho <martinembon@hotmail.com>
2021-11-26 21:36:32 -0300

    projeto final gama

--- controllers/institucionalController.js
+++ controllers/institucionalController.js
@@ -1,7 +1,17 @@
 const institucionalController = {
     index:(req,res)=>{
         return res.render("index",{title:"Gama Projeto Final"})
-    }
+    },
+    cadastrar:(req,res)=>{
+        return res.render("cadastro-curso",{title:"Gama Projeto Final"})
+    },
+    listar:(req,res)=>{
+        return res.render("listar-curso",{title:"Gama Projeto Final"})
+    },
+    admin:(req,res)=>{
+        return res.render("admin-cursos",{title:"Gama Projeto Final"})
+    },
+    
 }
 
 
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,5 +3,8 @@ const router = express.Router()
 const institucionalController = require("../controllers/institucionalController")
 
 router.get("/",institucionalController.index)
+router.get("/cadastro",institucionalController.cadastrar)
+router.get("/cursos",institucionalController.listar)
+router.get("/admin",institucionalController.admin)
 
 module.exports = router
